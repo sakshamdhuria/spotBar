4d1c6634b39800853bf21d622cb79a1c8306fed0
Saksham Dhuria <107601836+sakshamdhuria@users.noreply.github.com>
2025-08-29 19:51:50 -0700

    Initial Commit

---
+++ SpotBar/SpotBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotBarApp.swift
+//  SpotBar
+//
+//  Created by Shrey Dhuria on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
